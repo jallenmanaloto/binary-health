ee64cc27f521cf91d0226284a4d5f54522c44ca2
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-01-18 17:15:23 +0800

    Add login post request to api to create session for user

--- src/components/login/UserLogin.js
+++ src/components/login/UserLogin.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import { loginStyle } from "../styles/Styles";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
@@ -10,13 +11,45 @@ import Typography from "@mui/material/Typography";
 
 const UserLogin = () => {
   const login = loginStyle();
+  //Declaring states for textfields value
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  //Handling onchange for value of textfields
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  //Handling login post request to api
+  const handleLogin = (e) => {
+    e.preventDefault();
+    axios({
+      method: "POST",
+      url: "http://localhost:3001/auth/sign_in",
+      data: {
+        email: email,
+        password: password,
+      },
+    })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
+
+  //handling login on enter key
+  const handleEnterKeyLogin = (e) => {
+    if (e.key === "Enter") handleLogin(e);
+  };
 
   return (
     <Box>
       <Grid container direction="row" spacing={2}>
         <Grid item xs={12} xl={12}>
           <Paper className={login.form} elevation={2}>
-            <Grid container direction="column" xs={8} sm={8} md={6} xl={3}>
+            <Grid container item direction="column" xs={8} sm={8} md={6} xl={3}>
               <Grid item>
                 <Typography sx={{ paddingBottom: "2em" }} variant="h3">
                   BETELGEUSE
@@ -32,16 +65,23 @@ const UserLogin = () => {
                 </Typography>
               </Grid>
               <TextField
+                onChange={handleEmail}
+                onKeyDown={handleEnterKeyLogin}
+                value={email}
                 sx={{ paddingBottom: "1.2rem", marginTop: "2.7em" }}
                 label="Email"
                 variant="outlined"
               />
               <TextField
+                onChange={handlePassword}
+                onKeyDown={handleEnterKeyLogin}
+                value={password}
                 sx={{ paddingBottom: "1.2rem" }}
                 label="Password"
                 variant="outlined"
               />
               <Button
+                onClick={handleLogin}
                 sx={{ height: "3.2em", fontWeight: "bold" }}
                 variant="contained"
               >
