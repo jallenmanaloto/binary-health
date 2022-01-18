882f65f24059cf73e18815d7f5ad00debd7cb1ee
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-01-18 19:48:35 +0800

    Created states for current user's details and headers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { useState } from "react";
 import UserLogin from "./components/login/UserLogin";
+import CurrentUser from "./components/auth/CurrentUser";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState({
+    email: "",
+    first_name: "",
+    middle_name: "",
+    last_name: "",
+    covid_status: "",
+    role: "",
+  });
+
+  const [headers, setHeaders] = useState({
+    token: "",
+    client: "",
+    expiry: "",
+    uid: "",
+  });
+
   return (
     <div className="App">
-      <UserLogin />
+      <CurrentUser.Provider
+        value={{ currentUser, setCurrentUser, headers, setHeaders }}
+      >
+        <UserLogin />
+      </CurrentUser.Provider>
     </div>
   );
 }
