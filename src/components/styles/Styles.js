04bc247413545fd2f950a82498f98f81255efaed
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-01-19 21:41:27 +0800

    Created style for Appbar component

--- src/components/styles/Styles.js
+++ src/components/styles/Styles.js
@@ -1,6 +1,45 @@
 import React from "react";
 import { makeStyles } from "@mui/styles";
 
+export const adminStyle = makeStyles({
+  root: {
+    display: "flex",
+    height: "100vh",
+    width: "100vw",
+    backgroundColor: "#f5f5f5",
+  },
+});
+
+export const appbarStyle = makeStyles({
+  root: {
+    width: "87vw",
+  },
+  accountButton: {
+    position: "absolute",
+    right: "4em",
+  },
+  accountCircle: {
+    color: "whitesmoke",
+  },
+});
+
+export const drawerStyle = makeStyles({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    height: "100vh",
+    width: "13vw",
+    backgroundColor: "white",
+    borderRight: "1px solid #e0e0e0",
+  },
+  expandIcon: {
+    color: "#707070",
+  },
+  navigationText: {
+    color: "#575757",
+  },
+});
+
 export const formStyle = makeStyles({
   display: {
     backgroundColor: "lightcoral",
@@ -37,30 +76,5 @@ export const formStyle = makeStyles({
     justifyContent: "center",
     width: "100%",
     paddingTop: "1rem",
-  },
-});
-
-export const adminStyle = makeStyles({
-  root: {
-    height: "100vh",
-    width: "100vw",
-    backgroundColor: "#f5f5f5",
-  },
-});
-
-export const drawerStyle = makeStyles({
-  root: {
-    display: "flex",
-    flexDirection: "column",
-    height: "100vh",
-    width: "13vw",
-    backgroundColor: "white",
-    borderRight: "1px solid #e0e0e0",
-  },
-  expandIcon: {
-    color: "#707070",
-  },
-  navigationText: {
-    color: "#575757",
   },
 });
