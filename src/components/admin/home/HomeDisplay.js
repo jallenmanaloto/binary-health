3f57c0dd6c967485b6655ec2d64451151ca5579b
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-01-20 15:36:51 +0800

    Add Cases component

--- src/components/admin/home/HomeDisplay.js
+++ src/components/admin/home/HomeDisplay.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { homeDisplayStyle } from "../../styles/Styles";
+import Cases from "./Cases";
 
 const HomeDisplay = () => {
   const homeStyle = homeDisplayStyle();
-  return <div className={homeStyle.root}>hello</div>;
+  return (
+    <div className={homeStyle.root}>
+      hello
+      <Cases />
+    </div>
+  );
 };
 
 export default HomeDisplay;
