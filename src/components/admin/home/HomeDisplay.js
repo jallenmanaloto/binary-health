4bfad8ce619ebd2c452c187e61215d675e3fa8a5
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-01-20 16:29:03 +0800

    Add Charts component

--- src/components/admin/home/HomeDisplay.js
+++ src/components/admin/home/HomeDisplay.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { homeDisplayStyle } from "../../styles/Styles";
 import Cases from "./Cases";
+import Charts from "./Charts";
 
 const HomeDisplay = () => {
   const homeStyle = homeDisplayStyle();
   return (
     <div className={homeStyle.root}>
-      hello
       <Cases />
+      <Charts />
     </div>
   );
 };
