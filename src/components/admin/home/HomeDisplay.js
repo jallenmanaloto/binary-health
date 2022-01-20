396ec39107b6d8c6ec658c0dd6549bb8cba06b39
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-01-20 19:42:27 +0800

    Removed style layouts

--- src/components/admin/home/HomeDisplay.js
+++ src/components/admin/home/HomeDisplay.js
@@ -1,27 +1,29 @@
 import React from "react";
-import { homeDisplayStyle } from "../../styles/Styles";
 import Cases from "./Cases";
 import Container from "@mui/material/Container";
 import Charts from "./Charts";
 import Grid from "@mui/material/Grid";
+import Users from "./Users";
 
 const HomeDisplay = () => {
-  const homeStyle = homeDisplayStyle();
   return (
     <Container
+      maxWidth="lg"
       sx={{
         position: "absolute",
         top: "10vh",
         right: "0",
         bottom: "0",
         left: { xs: "0", sm: "0", md: "30vw", lg: "30vw", xl: "13vw" },
+        overflowX: "hidden",
+        overflowY: "scroll",
       }}
-      className={homeStyle.root}
     >
       <Grid container>
         <Grid item sm={11} md={6} lg={12}>
           <Cases />
           <Charts />
+          <Users />
         </Grid>
       </Grid>
     </Container>
