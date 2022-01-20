35039394e9bb81849a0467885a93ea8d6c71d4bb
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-01-20 16:45:49 +0800

    Add responsiveness on breakpoints

--- src/components/admin/home/HomeDisplay.js
+++ src/components/admin/home/HomeDisplay.js
@@ -1,15 +1,30 @@
 import React from "react";
 import { homeDisplayStyle } from "../../styles/Styles";
 import Cases from "./Cases";
+import Container from "@mui/material/Container";
 import Charts from "./Charts";
+import Grid from "@mui/material/Grid";
 
 const HomeDisplay = () => {
   const homeStyle = homeDisplayStyle();
   return (
-    <div className={homeStyle.root}>
-      <Cases />
-      <Charts />
-    </div>
+    <Container
+      sx={{
+        position: "absolute",
+        top: "10vh",
+        right: "0",
+        bottom: "0",
+        left: { xs: "0", sm: "0", md: "30vw", lg: "30vw", xl: "13vw" },
+      }}
+      className={homeStyle.root}
+    >
+      <Grid container>
+        <Grid item sm={11} md={6} lg={12}>
+          <Cases />
+          <Charts />
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
