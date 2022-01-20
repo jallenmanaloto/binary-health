f8c13407349aa5b578b18f377f2968881b8381d4
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-01-20 14:34:37 +0800

    Created display for Home navigation

---
+++ src/components/admin/HomeDisplay.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { homeDisplayStyle } from "../styles/Styles";
+
+const HomeDisplay = () => {
+  const homeStyle = homeDisplayStyle();
+  return <div className={homeStyle.root}>hello</div>;
+};
+
+export default HomeDisplay;
