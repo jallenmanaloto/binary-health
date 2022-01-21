020726798db90958802bbad710d09a2f67b7ef21
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-01-21 14:17:18 +0800

    Created Establishments component

---
+++ src/components/admin/home/Establishments.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+
+const Establishments = () => {
+  return <div></div>;
+};
+
+export default Establishments;
