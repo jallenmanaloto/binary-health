f3ef8894e72568d892733cd2014ec87501569c66
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-01-21 18:32:57 +0800

    Add Cases component for navigation

--- src/components/admin/Admin.js
+++ src/components/admin/Admin.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { adminStyle } from "../styles/Styles";
 import AdminNavigation from "./context/AdminNavigations";
 import Appbar from "./Appbar";
+import Cases from "./cases/Cases";
 import Drawer from "./Drawer";
 import Establishments from "./establishment/Establishments";
 import HomeDisplay from "./home/HomeDisplay";
@@ -18,6 +19,7 @@ const Admin = () => {
         {navigation === "home" ? <HomeDisplay /> : null}
         {navigation === "request" ? <Requests /> : null}
         {navigation === "establishment" ? <Establishments /> : null}
+        {navigation === "cases" ? <Cases /> : null}
       </AdminNavigation.Provider>
     </div>
   );
