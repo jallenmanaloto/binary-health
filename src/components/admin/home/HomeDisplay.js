9efd890851cbe8fc4c0ed9a8ffa1193c490df0f2
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-01-21 18:06:13 +0800

    Fixed layout for responsiveness across screens

--- src/components/admin/home/HomeDisplay.js
+++ src/components/admin/home/HomeDisplay.js
@@ -2,31 +2,35 @@ import React from "react";
 import Cases from "./Cases";
 import Container from "@mui/material/Container";
 import Charts from "./Charts";
+import Establishments from "../establishment/Establishments";
 import Grid from "@mui/material/Grid";
+import Requests from "../request/Requests";
 import Users from "./Users";
+import { Paper } from "@mui/material";
 
 const HomeDisplay = () => {
   return (
-    <Container
-      maxWidth="lg"
+    <Paper
       sx={{
-        position: "absolute",
+        position: "fixed",
         top: "10vh",
         right: "0",
         bottom: "0",
-        left: { xs: "0", sm: "0", md: "30vw", lg: "30vw", xl: "13vw" },
+        left: { xs: "0", sm: "0", md: "25vw", lg: "25vw", xl: "13vw" },
         overflowX: "hidden",
         overflowY: "scroll",
+        backgroundColor: "transparent",
       }}
+      elevation={0}
     >
-      <Grid container>
-        <Grid item sm={11} md={6} lg={12}>
+      <Grid container justifyContent="center">
+        <Grid item sm={11} md={12} lg={12} xl={12}>
           <Cases />
           <Charts />
           <Users />
         </Grid>
       </Grid>
-    </Container>
+    </Paper>
   );
 };
 
