45279c82efc7e3469e08b56a80643bdaaaf3bfdc
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-01-21 18:04:56 +0800

    Implemented navigation on different components

--- src/components/admin/Admin.js
+++ src/components/admin/Admin.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { adminStyle } from "../styles/Styles";
+import AdminNavigation from "./context/AdminNavigations";
 import Appbar from "./Appbar";
 import Drawer from "./Drawer";
+import Establishments from "./establishment/Establishments";
 import HomeDisplay from "./home/HomeDisplay";
+import Requests from "./request/Requests";
 
 const Admin = () => {
   const adminTheme = adminStyle();
+  const [navigation, setNavigation] = useState("home");
   return (
     <div className={adminTheme.root}>
-      <Drawer />
-      <Appbar />
-      <HomeDisplay />
+      <AdminNavigation.Provider value={{ navigation, setNavigation }}>
+        <Drawer />
+        <Appbar />
+        {navigation === "home" ? <HomeDisplay /> : null}
+        {navigation === "request" ? <Requests /> : null}
+        {navigation === "establishment" ? <Establishments /> : null}
+      </AdminNavigation.Provider>
     </div>
   );
 };
