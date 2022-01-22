be564ce87d9679a8202ef2cd2f61f010e62c78da
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-01-22 17:18:30 +0800

    Add Appbar to component

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -2,11 +2,13 @@ import React from "react";
 import { userStyle } from "../styles/Styles";
 import Grid from "@mui/material/Grid";
 import UserAppbar from "./appbar/UserBottomAppbar";
+import UserTopAppbar from "./appbar/UserTopAppbar";
 
 const User = () => {
   const classes = userStyle();
   return (
     <div className={classes.root}>
+      <UserTopAppbar />
       <UserAppbar />
     </div>
   );
