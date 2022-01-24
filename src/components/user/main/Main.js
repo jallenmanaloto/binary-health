a4e4d47dd33d382fdb1851875be9e3069c170ba9
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-01-24 15:37:38 +0800

    Fixed padding layout for better view

--- src/components/user/main/Main.js
+++ src/components/user/main/Main.js
@@ -8,7 +8,7 @@ const Main = () => {
       maxWidth="xl"
       sx={{
         minHeight: "100vh",
-        pt: { xs: 0, sm: 10, md: 10, lg: 10 },
+        pt: { xs: 0, sm: 5, md: 5, lg: 5 },
         overflowY: "scroll",
       }}
     >
