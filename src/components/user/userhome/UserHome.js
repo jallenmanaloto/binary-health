2ad72d3c609eedfdb82f32dec5c8d249eede7163
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-01-24 15:38:47 +0800

    Add style layout and Headlines component

--- src/components/user/userhome/UserHome.js
+++ src/components/user/userhome/UserHome.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { userStyle } from "../../styles/Styles";
+import Headlines from "./Headlines";
 import MyAppointments from "../calendars/MyAppointments";
 import Requests from "./Requests";
 
 const UserHome = () => {
+  //setting style layout
+  const classes = userStyle();
   return (
-    <div>
+    <div className={classes.homeRoot}>
       <MyAppointments />
       <Requests />
+      <Headlines />
     </div>
   );
 };
