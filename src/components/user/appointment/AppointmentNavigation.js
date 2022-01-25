5d2d125247553b5760c0effc08ca715e294c2613
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-01-25 14:17:08 +0800

    Rename component for readability and add functions to handle additional navigations

--- src/components/user/appointment/AppointmentNavigation.js
+++ src/components/user/appointment/AppointmentNavigation.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import ArticleIcon from "@mui/icons-material/Article";
 import Divider from "@mui/material/Divider";
 import EventNoteIcon from "@mui/icons-material/EventNote";
 import LibraryAddCheckIcon from "@mui/icons-material/LibraryAddCheck";
@@ -7,29 +8,64 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import Paper from "@mui/material/Paper";
+import PostAddIcon from "@mui/icons-material/PostAdd";
 import Typography from "@mui/material/Typography";
 
-const AppointmentNavigation = ({
+const ScheduleAppointmentNavigation = ({
   setMyAppointmentsActive,
   setMakeAppointmentsActive,
+  setMyRequestsActive,
+  setMakeRequestsActive,
 }) => {
   //setting state for navigation of appointment
   const [myAppointments, setMyAppointments] = useState(true);
   const [makeAppointments, setMakeAppointments] = useState(false);
+  const [myRequests, setMyRequests] = useState(false);
+  const [makeRequests, setMakeRequests] = useState(false);
 
   //handling active navigation
   const handleMyAppointments = () => {
     setMyAppointments(true);
     setMakeAppointments(false);
+    setMyRequests(false);
+    setMakeRequests(false);
     setMyAppointmentsActive(true);
     setMakeAppointmentsActive(false);
+    setMyRequestsActive(false);
+    setMakeRequestsActive(false);
   };
 
   const handleMakeAppointments = () => {
     setMyAppointments(false);
     setMakeAppointments(true);
+    setMyRequests(false);
+    setMakeRequests(false);
     setMyAppointmentsActive(false);
     setMakeAppointmentsActive(true);
+    setMyRequestsActive(false);
+    setMakeRequestsActive(false);
+  };
+
+  const handleMyRequests = () => {
+    setMyAppointments(false);
+    setMakeAppointments(false);
+    setMyRequests(true);
+    setMakeRequests(false);
+    setMyAppointmentsActive(false);
+    setMakeAppointmentsActive(true);
+    setMyRequestsActive(true);
+    setMakeRequestsActive(false);
+  };
+
+  const handleCreateRequests = () => {
+    setMyAppointments(false);
+    setMakeAppointments(false);
+    setMyRequests(false);
+    setMakeRequests(true);
+    setMyAppointmentsActive(false);
+    setMakeAppointmentsActive(false);
+    setMyRequestsActive(false);
+    setMakeRequestsActive(true);
   };
 
   return (
@@ -46,17 +82,15 @@ const AppointmentNavigation = ({
           sx={{
             pt: 2,
             color: "#3376b5",
-            fontWeight: 600,
+            fontWeight: 400,
             width: "90%",
-            textAlign: "center",
           }}
           variant="h6"
           component="h4"
         >
           Appointment
-          <Divider sx={{ mt: 1, backgroundColor: "#3376b5" }} />
         </Typography>
-        <List sx={{ mt: 15, width: "100%" }} component="nav">
+        <List sx={{ mt: 0, width: "100%" }} component="nav">
           <ListItemButton
             sx={{ display: "flex", justifyContent: "start" }}
             onClick={handleMyAppointments}
@@ -114,9 +148,80 @@ const AppointmentNavigation = ({
             )}
           </ListItemButton>
         </List>
+
+        <Typography
+          sx={{
+            pt: 2,
+            color: "#3376b5",
+            fontWeight: 400,
+            width: "90%",
+          }}
+          variant="h6"
+          component="h4"
+        >
+          Requests
+        </Typography>
+        <List sx={{ mt: 0, width: "100%" }} component="nav">
+          <ListItemButton
+            sx={{ display: "flex", justifyContent: "start" }}
+            onClick={handleMyRequests}
+          >
+            <ListItemIcon>
+              {myRequests ? (
+                <ArticleIcon sx={{ ml: 2, color: "#3376b5" }} />
+              ) : (
+                <ArticleIcon sx={{ ml: 2 }} />
+              )}
+            </ListItemIcon>
+            {myRequests ? (
+              <Typography
+                sx={{ fontSize: "1.1rem", color: "#3376b5" }}
+                variant="h6"
+                component="h6"
+              >
+                My Requests
+              </Typography>
+            ) : (
+              <Typography
+                sx={{ fontSize: "1.1rem", color: "gray" }}
+                variant="h6"
+              >
+                My Requests
+              </Typography>
+            )}
+          </ListItemButton>
+          <ListItemButton
+            sx={{ display: "flex", justifyContent: "start" }}
+            onClick={handleCreateRequests}
+          >
+            <ListItemIcon>
+              {makeRequests ? (
+                <PostAddIcon sx={{ ml: 2, color: "#3376b5" }} />
+              ) : (
+                <PostAddIcon sx={{ ml: 2 }} />
+              )}
+            </ListItemIcon>
+            {makeRequests ? (
+              <Typography
+                sx={{ fontSize: "1.1rem", color: "#3376b5" }}
+                variant="h6"
+                component="h6"
+              >
+                Create Request
+              </Typography>
+            ) : (
+              <Typography
+                sx={{ fontSize: "1.1rem", color: "gray" }}
+                variant="h6"
+              >
+                Create Request
+              </Typography>
+            )}
+          </ListItemButton>
+        </List>
       </Paper>
     </div>
   );
 };
 
-export default AppointmentNavigation;
+export default ScheduleAppointmentNavigation;
