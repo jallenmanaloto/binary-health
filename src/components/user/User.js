82ca5e320e6c9a85da96ed6a4d940b384cd05da7
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-01-25 14:49:05 +0800

    Add context provider and values for navigation with appbar

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { userStyle } from "../styles/Styles";
-import Grid from "@mui/material/Grid";
+import AppbarNavigation from "./context/AppbarNavigation";
 import Main from "./main/Main";
 import UserAppbar from "./appbar/UserBottomAppbar";
 import UserTopAppbar from "./appbar/UserTopAppbar";
 
 const User = () => {
   const classes = userStyle();
+
+  //handling navigations for appbar
+  const [home, setHome] = useState(true);
+  const [scheduleRequests, setScheduleRequests] = useState(false);
+
   return (
-    <div className={classes.root}>
-      <UserTopAppbar />
-      <Main />
-      <UserAppbar />
-    </div>
+    <AppbarNavigation.Provider
+      value={{ home, setHome, scheduleRequests, setScheduleRequests }}
+    >
+      <div className={classes.root}>
+        <UserTopAppbar />
+        <Main />
+        <UserAppbar />
+      </div>
+    </AppbarNavigation.Provider>
   );
 };
 
