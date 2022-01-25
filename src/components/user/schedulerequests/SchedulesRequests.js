50e770f64b1c91be671358dba33cc9ab31b9ecb7
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-01-25 21:35:56 +0800

    Add context for navigation

--- src/components/user/schedulerequests/SchedulesRequests.js
+++ src/components/user/schedulerequests/SchedulesRequests.js
@@ -1,30 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { ThemeProvider, useTheme } from "@mui/material/styles";
+import AppbarNavigation from "../context/AppbarNavigation";
 import AppointmentDisplay from "./AppointmentDisplay";
 import CreateRequest from "./CreateRequest";
-import ScheduleRequestsNavigation from "./ScheduleRequestsNavigation";
-import MakeAppointment from "./MakeAppointment";
-import MyRequests from "./MyRequests";
 import Container from "@mui/material/Container";
+import DrawerLeft from "./DrawerLeft";
 import Grid from "@mui/material/Grid";
+import MakeAppointment from "./MakeAppointment";
+import MenuIcon from "@mui/icons-material/Menu";
+import MyRequests from "./MyRequests";
+import ScheduleRequestContext from "../context/ScheduleRequestContext";
+import ScheduleRequestsNavigation from "./ScheduleRequestsNavigation";
 
 const SchedulesRequests = () => {
   const theme = useTheme();
   //setting state for upstream value of navigations
-  const [myAppointmentsActive, setMyAppointmentsActive] = useState(true);
-  const [makeAppointmentsActive, setMakeAppointmentsActive] = useState(false);
-  const [myRequestsActive, setMyRequestsActive] = useState(false);
-  const [makeRequestsActive, setMakeRequestsActive] = useState(false);
+  // const [myAppointmentsActive, setMyAppointmentsActive] = useState(true);
+  // const [makeAppointmentsActive, setMakeAppointmentsActive] = useState(false);
+  // const [myRequestsActive, setMyRequestsActive] = useState(false);
+  // const [makeRequestsActive, setMakeRequestsActive] = useState(false);
 
+  const {
+    myAppointmentsActive,
+    makeAppointmentsActive,
+    myRequestsActive,
+    makeRequestsActive,
+  } = useContext(ScheduleRequestContext);
+
+  const { setMenuActive } = useContext(AppbarNavigation);
   return (
     <ThemeProvider theme={theme}>
       <Container
         sx={{
-          mt: { xs: 2, sm: 6, lg: 6 },
+          mt: { xs: 2, sm: 5, lg: 6 },
           height: "94vh",
         }}
         maxWidth="lg"
       >
+        <MenuIcon
+          sx={{
+            color: "gray",
+            fontSize: "2.25rem",
+            cursor: "pointer",
+            position: "fixed",
+            top: 18,
+            left: 12,
+            zIndex: "10",
+            display: { xs: "block", lg: "none" },
+          }}
+          onClick={() => setMenuActive(true)}
+        />
+        <DrawerLeft />
         <Grid container spacing={3}>
           <Grid
             sx={{
@@ -34,12 +60,7 @@ const SchedulesRequests = () => {
             md={0}
             lg={3}
           >
-            <ScheduleRequestsNavigation
-              setMyAppointmentsActive={setMyAppointmentsActive}
-              setMakeAppointmentsActive={setMakeAppointmentsActive}
-              setMyRequestsActive={setMyRequestsActive}
-              setMakeRequestsActive={setMakeRequestsActive}
-            />
+            <ScheduleRequestsNavigation />
           </Grid>
           <Grid
             sx={{ display: { md: "block" } }}
