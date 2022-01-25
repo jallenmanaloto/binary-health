2509553c4e8324524e05fad8b53abe8914645596
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-01-25 20:30:11 +0800

    Create SearchCountry component to hold search functions for travel restrictions

---
+++ src/components/user/restrictions/SearchCountry.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+
+const SearchCountry = () => {
+  return <div></div>;
+};
+
+export default SearchCountry;
