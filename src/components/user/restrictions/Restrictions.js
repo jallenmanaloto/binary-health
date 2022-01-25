b2874fe89b1e6fc5ba07c86ba933c15700fbca69
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-01-25 20:29:40 +0800

    Fixed layout of main component

--- src/components/user/restrictions/Restrictions.js
+++ src/components/user/restrictions/Restrictions.js
@@ -5,10 +5,10 @@ import Paper from "@mui/material/Paper";
 
 const Restrictions = () => {
   return (
-    <Container sx={{ backgroundColor: "red" }} maxWidth="lg">
-      <Grid container>
-        <Grid item lg={12}>
-          <Paper sx={{ mt: 5 }}>hello</Paper>
+    <Container sx={{ mt: { xs: 2, sm: 6, md: 6, lg: 6 } }} maxWidth="lg">
+      <Grid container justifyContent="center">
+        <Grid item xs={12} sm={12} md={12} lg={12}>
+          <Paper sx={{ height: "90vh" }}></Paper>
         </Grid>
       </Grid>
     </Container>
