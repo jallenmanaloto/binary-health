3beaab7454b7ae4b3f92254211390552166fd030
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-01-25 21:34:22 +0800

    Add context for requests navigation

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import { userStyle } from "../styles/Styles";
 import AppbarNavigation from "./context/AppbarNavigation";
+import DrawerLeft from "./schedulerequests/DrawerLeft";
 import Main from "./main/Main";
+import ScheduleRequestContext from "./context/ScheduleRequestContext";
 import UserAppbar from "./appbar/UserBottomAppbar";
 import UserTopAppbar from "./appbar/UserTopAppbar";
 
@@ -10,25 +12,48 @@ const User = () => {
 
   //handling navigations for appbar
   const [home, setHome] = useState(true);
-  const [scheduleRequests, setScheduleRequests] = useState(false);
+  const [menuActive, setMenuActive] = useState(false);
   const [restrictions, setRestrictions] = useState(false);
+  const [scheduleRequests, setScheduleRequests] = useState(false);
+
+  //hanndling navigations for requests
+  const [myAppointmentsActive, setMyAppointmentsActive] = useState(true);
+  const [makeAppointmentsActive, setMakeAppointmentsActive] = useState(false);
+  const [myRequestsActive, setMyRequestsActive] = useState(false);
+  const [makeRequestsActive, setMakeRequestsActive] = useState(false);
 
   return (
     <AppbarNavigation.Provider
       value={{
         home,
         setHome,
+        menuActive,
+        setMenuActive,
         restrictions,
         setRestrictions,
         scheduleRequests,
         setScheduleRequests,
       }}
     >
-      <div className={classes.root}>
-        <UserTopAppbar />
-        <Main />
-        <UserAppbar />
-      </div>
+      <ScheduleRequestContext.Provider
+        value={{
+          myAppointmentsActive,
+          setMyAppointmentsActive,
+          makeAppointmentsActive,
+          setMakeAppointmentsActive,
+          myRequestsActive,
+          setMyRequestsActive,
+          makeRequestsActive,
+          setMakeRequestsActive,
+        }}
+      >
+        <div className={classes.root}>
+          <UserTopAppbar />
+          <DrawerLeft />
+          <Main />
+          <UserAppbar />
+        </div>
+      </ScheduleRequestContext.Provider>
     </AppbarNavigation.Provider>
   );
 };
