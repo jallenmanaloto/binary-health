e9c4787c76f4a65f86e6069d17466cfd888439f9
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-01-25 16:40:48 +0800

    Fixed layout of component for consistent alignment

--- src/components/user/schedulerequests/MyRequests.js
+++ src/components/user/schedulerequests/MyRequests.js
@@ -37,13 +37,11 @@ const MyRequests = () => {
       .catch((err) => console.log(err));
   }, []);
   return (
-    <Container maxWidth="lg">
-      <Grid container justifyContent="center">
-        <Grid item xs={12} lg={12}>
-          <RequestsTable columns={columns} rows={userRequest} />
-        </Grid>
+    <Grid container justifyContent="center">
+      <Grid item xs={12} lg={12}>
+        <RequestsTable columns={columns} rows={userRequest} />
       </Grid>
-    </Container>
+    </Grid>
   );
 };
 
