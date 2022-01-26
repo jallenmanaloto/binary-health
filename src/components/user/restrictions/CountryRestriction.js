ff264cd967fbdd88fc5902360e961749ae995688
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-01-26 16:04:12 +0800

    Created Country Restriction component to hold all responses from api

---
+++ src/components/user/restrictions/CountryRestriction.js
@@ -0,0 +1,9 @@
+import React, { useContext } from "react";
+import CountrySearch from "../context/CountrySearch";
+import Grid from "@mui/material/Grid";
+
+const CountryRestriction = () => {
+  return <div></div>;
+};
+
+export default CountryRestriction;
