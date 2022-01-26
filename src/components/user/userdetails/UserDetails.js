084dc5efcc216bd5c374a35995b5034fccab2ab8
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-01-26 20:47:38 +0800

    Created UserDetails component for user's account details

--- src/components/user/userdetails/UserDetails.js
+++ src/components/user/userdetails/UserDetails.js
@@ -1,7 +1,24 @@
 import React from "react";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import UserDetailsNavigation from "./UserDetailsNavigation";
 
 const UserDetails = () => {
-  return <div style={{ backgroundColor: "red", height: "60vh" }}>HELLO</div>;
+  return (
+    <Container
+      sx={{ mt: { xs: 2, sm: 5, lg: 6 }, height: "94vh" }}
+      maxWidth="lg"
+    >
+      <Grid container spacing={3}>
+        <Grid item md={0} lg={3}>
+          <UserDetailsNavigation />
+        </Grid>
+        <Grid item xs={12} lg={9}>
+          Content
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default UserDetails;
