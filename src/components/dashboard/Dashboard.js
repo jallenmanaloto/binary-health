58059c7ac25ee7cbd3a2fee00f20fab7e4505ce2
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-01-27 20:17:33 +0800

    Add use effect to get set data headers and user details from storage

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,10 +1,19 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import CurrentUser from "../auth/CurrentUser";
 import Admin from "../admin/Admin";
 import User from "../user/User";
 
 const Dashboard = () => {
-  const { currentUser } = useContext(CurrentUser);
+  const { currentUser, setCurrentUser, setHeaders } = useContext(CurrentUser);
+  useEffect(() => {
+    localStorage.getItem("userAuth")
+      ? setHeaders(JSON.parse(localStorage.getItem("userAuth")))
+      : setHeaders(JSON.parse(sessionStorage.getItem("userAuth")));
+
+    localStorage.getItem("userDetails")
+      ? setCurrentUser(JSON.parse(localStorage.getItem("userDetails")))
+      : setCurrentUser(JSON.parse(sessionStorage.getItem("userDetails")));
+  }, []);
   return (
     <div>
       {currentUser.role === "admin" ? <Admin /> : null}
