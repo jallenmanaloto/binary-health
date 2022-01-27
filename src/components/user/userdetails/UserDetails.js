690b622c538563bc9ffff09bdb1bb708a9728a1c
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-01-27 15:38:45 +0800

    Fixed layout in responsive view

--- src/components/user/userdetails/UserDetails.js
+++ src/components/user/userdetails/UserDetails.js
@@ -2,6 +2,7 @@ import React from "react";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import UserDetailsNavigation from "./UserDetailsNavigation";
+import UserPersonal from "./UserPersonal";
 
 const UserDetails = () => {
   return (
@@ -10,11 +11,18 @@ const UserDetails = () => {
       maxWidth="lg"
     >
       <Grid container spacing={3}>
-        <Grid item md={0} lg={3}>
+        <Grid
+          sx={{
+            display: { xs: "none", sm: "none", md: "none", lg: "block" },
+          }}
+          item
+          md={0}
+          lg={3}
+        >
           <UserDetailsNavigation />
         </Grid>
         <Grid item xs={12} lg={9}>
-          Content
+          <UserPersonal />
         </Grid>
       </Grid>
     </Container>
