ba784c005c595d3e950551c0d783e75ba7fa56e5
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-01-27 17:31:11 +0800

    Add context for navigation

--- src/components/user/userdetails/UserDetails.js
+++ src/components/user/userdetails/UserDetails.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
+import UserDetailsNav from "../context/UserDetailsNav";
 import UserDetailsNavigation from "./UserDetailsNavigation";
 import UserPersonal from "./UserPersonal";
+import QRCode from "./QRCode";
 
 const UserDetails = () => {
+  //setting context for navigation
+  const { activities, setActivities, personal, setPersonal, qr, setQr } =
+    useContext(UserDetailsNav);
+
   return (
     <Container
       sx={{ mt: { xs: 2, sm: 5, lg: 6 }, height: "94vh" }}
@@ -22,7 +28,9 @@ const UserDetails = () => {
           <UserDetailsNavigation />
         </Grid>
         <Grid item xs={12} lg={9}>
-          <UserPersonal />
+          {personal ? <UserPersonal /> : null}
+          {qr ? <QRCode /> : null}
+          {/* {personal ? <UserPersonal /> : null} */}
         </Grid>
       </Grid>
     </Container>
