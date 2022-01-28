1d40a3445b336b57edb48450be52cc39ef8e5a52
jallenmanaloto <84300846+jallenmanaloto@users.noreply.github.com>
2022-01-28 18:57:03 +0800

    Implemented logout function

--- src/components/user/appbar/UserTopAppbar.js
+++ src/components/user/appbar/UserTopAppbar.js
@@ -1,15 +1,21 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import CurrentUser from "../../auth/CurrentUser";
 import AppBar from "@mui/material/AppBar";
 import AppbarNavigation from "../context/AppbarNavigation";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import AssignmentIcon from "@mui/icons-material/Assignment";
 import AssignmentOutlinedIcon from "@mui/icons-material/AssignmentOutlined";
+import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import HomeIcon from "@mui/icons-material/Home";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 import TodayIcon from "@mui/icons-material/Today";
 import TodayOutlinedIcon from "@mui/icons-material/TodayOutlined";
 import Toolbar from "@mui/material/Toolbar";
@@ -28,6 +34,43 @@ const UserTopAppbar = () => {
     userDetails,
     setUserDetails,
   } = useContext(AppbarNavigation);
+
+  //setting context for currentUser logged in
+  const { currentUser, headers } = useContext(CurrentUser);
+
+  //declaring navigation for logout
+  let navigate = useNavigate();
+
+  //setting anchor for menu
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleMenuOpen = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+  const handleCloseMenu = () => {
+    setAnchorEl(null);
+  };
+
+  //handling logout
+  const handleLogout = () => {
+    axios({
+      method: "DELETE",
+      url: "https://health-users-api.herokuapp.com/auth/sign_out",
+      headers: {
+        "access-token": headers.token,
+        client: headers.client,
+        expiry: headers.expiry,
+        uid: headers.uid,
+      },
+    })
+      .then(navigate("/login"))
+      .catch((err) => {
+        console.log(err);
+      });
+    localStorage.removeItem("userDetails");
+    localStorage.removeItem("userAuth");
+    localStorage.removeItem("amadeusToken");
+  };
 
   //handling active navigations
   const handleHomeNav = () => {
@@ -68,7 +111,7 @@ const UserTopAppbar = () => {
       >
         <Toolbar sx={{ p: 1 }}>
           <Container maxWidth="md">
-            <Grid container justifyContent="space-evenly" spacing={0}>
+            <Grid container item justifyContent="space-evenly" spacing={0}>
               <Grid item md={3}>
                 <Tooltip title={<h3>Home</h3>}>
                   <IconButton
@@ -148,6 +191,37 @@ const UserTopAppbar = () => {
               </Grid>
             </Grid>
           </Container>
+          <Button
+            sx={{
+              position: "fixed",
+              top: 18,
+              right: 50,
+              zIndex: 10,
+              color: "#3376b5",
+              display: {
+                xs: "none",
+                md: "none",
+                lg: "block",
+              },
+            }}
+            variant="text"
+            onClick={handleMenuOpen}
+          >
+            {`${currentUser.first_name} ${currentUser.last_name}`}
+          </Button>
+          <Menu
+            PaperProps={{
+              style: {
+                width: 230,
+              },
+            }}
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleCloseMenu}
+          >
+            <MenuItem onClick={handleCloseMenu}>Settings</MenuItem>
+            <MenuItem onClick={handleLogout}>Logout</MenuItem>
+          </Menu>
         </Toolbar>
       </AppBar>
     </div>
